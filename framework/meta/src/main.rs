37792159a692f538a6f3ba9a923ffc52339aa08b
Márcio Júnior <marcio.mj70@gmail.com>
2024-04-17 15:51:37 -0300

    chore: remove comments

--- framework/meta/src/main.rs
+++ framework/meta/src/main.rs
@@ -1,7 +1,3 @@
-// #[cfg(all(target_env="musl", target_pointer_width = "64"))]
-// #[global_allocator]
-// static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
 #[tokio::main]
 async fn main() {
     klever_sc_meta::cli_main_standalone().await;
