79ff779073d534ab918299ed0e2e1dc6c17e705c
nickgs1337 <nicklatim@live.com>
2024-05-22 10:35:25 -0300

    Make NULL_ID public in addressToId mapper

--- framework/base/src/storage/mappers/mod.rs
+++ framework/base/src/storage/mappers/mod.rs
@@ -14,7 +14,7 @@ mod vec_mapper;
 mod whitelist_mapper;
 mod address_to_id_mapper;
 
-pub use address_to_id_mapper::{AddressId, AddressToIdMapper};
+pub use address_to_id_mapper::{AddressToIdMapper, AddressId, NULL_ID};
 pub use bi_di_mapper::BiDiMapper;
 pub use linked_list_mapper::{LinkedListMapper, LinkedListNode};
 pub use map_mapper::MapMapper;
