be5de6f7686c1e59a49aadf473824e5624324211
nickgs1337 <nicklatim@live.com>
2024-05-22 09:26:50 -0300

    Create flag to make optional cargo check after upgrade

--- framework/meta/src/cmd/standalone/upgrade/upgrade_selector.rs
+++ framework/meta/src/cmd/standalone/upgrade/upgrade_selector.rs
@@ -3,6 +3,7 @@ use crate::{
     folder_structure::{dir_pretty_print, RelevantDirectories, RelevantDirectory},
     version_history::{versions_iter, LAST_UPGRADE_VERSION, VERSIONS},
 };
+use crate::cmd::standalone::upgrade::upgrade_settings::UpgradeSettings;
 
 use super::{
     upgrade_0_31::upgrade_to_31_0,
@@ -18,6 +19,8 @@ pub fn upgrade_sc(args: &UpgradeArgs) {
     } else {
         "./"
     };
+
+    let settings = UpgradeSettings::new(args.no_check);
 
     let last_version = args
         .override_target_version
@@ -51,7 +54,7 @@ pub fn upgrade_sc(args: &UpgradeArgs) {
         }
 
         for dir in dirs.iter_version(from_version) {
-            upgrade_post_processing(dir);
+            upgrade_post_processing(dir, &settings);
         }
 
         // // change the version in memory for the next iteration (dirs is not reloaded from disk)
@@ -82,19 +85,19 @@ fn upgrade_function_selector(dir: &RelevantDirectory) {
     }
 }
 
-fn upgrade_post_processing(dir: &RelevantDirectory) {
+fn upgrade_post_processing(dir: &RelevantDirectory, settings: &UpgradeSettings) {
     match dir.upgrade_in_progress {
         Some((_, "0.28.0")) | Some((_, "0.29.0")) | Some((_, "0.30.0")) | Some((_, "0.31.0"))
         | Some((_, "0.32.0")) | Some((_, "0.33.0")) | Some((_, "0.34.0")) | Some((_, "0.35.0"))
         | Some((_, "0.36.0")) | Some((_, "0.37.0")) | Some((_, "0.40.0")) | Some((_, "0.41.0"))
         | Some((_, "0.42.0")) | Some((_, "0.43.0")) => {
             print_post_processing(dir);
-            cargo_check(dir);
+            cargo_check(dir, settings);
         },
         Some((_, "0.39.0")) => {
             print_post_processing(dir);
             postprocessing_after_39_0(dir);
-            cargo_check(dir);
+            cargo_check(dir, settings);
         },
         _ => {},
     }
