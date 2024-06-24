6a8ca4e9d703a5811787055014a04c84dc9287fe
Emerson Barros <emersonfb99@gmail.com>
2024-06-24 16:53:13 -0300

    chore: better formatting

--- contracts/examples/deposit-kda-pool/src/deposit.rs
+++ contracts/examples/deposit-kda-pool/src/deposit.rs
@@ -2,7 +2,6 @@
 
 use klever_sc::imports::*;
 
-
 #[klever_sc::contract]
 pub trait DepositKDAPoolContract {
     #[init]
@@ -17,15 +16,19 @@ pub trait DepositKDAPoolContract {
     #[payable("*")]
     #[endpoint(deposit)]
     fn deposit(&self) {
-
         let (token_identifier, payment) = self.call_value().klv_or_single_fungible_kda();
         require!(
-            token_identifier == TokenIdentifier::from("KLV") ||  token_identifier == self.pool_id().get(),
+            token_identifier == TokenIdentifier::from("KLV")
+                || token_identifier == self.pool_id().get(),
             "payment token must be in KLV or defined pool ID"
         );
 
         require!(payment > 0, "payment amount can't be zero");
 
-        self.send().deposit_kda_pool(&self.pool_id().get().into_managed_buffer(), &token_identifier, &BigUint::from(payment))
+        self.send().deposit_kda_pool(
+            &self.pool_id().get().into_managed_buffer(),
+            &token_identifier,
+            &BigUint::from(payment),
+        )
     }
 }
