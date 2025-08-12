a49a2e0635fb787426b41df7e7ca060693641d10
phcarneirobc <peddrohenriquebc@gmail.com>
2025-08-12 17:32:44 -0300

    feat: implement admin added and removed events

--- contracts/modules/src/only_admin.rs
+++ contracts/modules/src/only_admin.rs
@@ -10,15 +10,25 @@ pub trait OnlyAdminModule {
     #[only_owner]
     #[endpoint(addAdmin)]
     fn add_admin(&self, address: ManagedAddress) {
-        self.admins().insert(address);
-        // TODO: event
+        let caller = self.blockchain().get_caller();
+        let epoch = self.blockchain().get_block_epoch();
+        let timestamp = self.blockchain().get_block_timestamp();
+
+        self.admins().insert(address.clone());
+
+        self.admin_added_event(&caller, epoch, timestamp, &address);
     }
 
     #[only_owner]
     #[endpoint(removeAdmin)]
     fn remove_admin(&self, address: ManagedAddress) {
+        let caller = self.blockchain().get_caller();
+        let epoch = self.blockchain().get_block_epoch();
+        let timestamp = self.blockchain().get_block_timestamp();
+
         self.admins().swap_remove(&address);
-        // TODO: event
+
+        self.admin_removed_event(&caller, epoch, timestamp, &address);
     }
 
     #[view(getAdmins)]
@@ -31,4 +41,22 @@ pub trait OnlyAdminModule {
             "Endpoint can only be called by admins"
         );
     }
+
+    #[event("admin_added")]
+    fn admin_added_event(
+        &self,
+        #[indexed] caller: &ManagedAddress,
+        #[indexed] epoch: u64,
+        #[indexed] timestamp: u64,
+        #[indexed] new_admin: &ManagedAddress,
+    );
+
+    #[event("admin_removed")]
+    fn admin_removed_event(
+        &self,
+        #[indexed] caller: &ManagedAddress,
+        #[indexed] epoch: u64,
+        #[indexed] timestamp: u64,
+        #[indexed] removed_admin: &ManagedAddress,
+    );
 }
