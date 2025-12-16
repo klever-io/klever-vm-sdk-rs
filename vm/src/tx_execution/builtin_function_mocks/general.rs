618069f7738a42bc8ac00bded233afc4ef3aede1
Beroni <beronimagalhaes@gmail.com>
2025-12-16 14:09:50 -0300

    [KLC-1874] Add Create Asset mock (#19)
    * chore: add mock of KleverCreateAsset in rust tests
    
    * chore: add create asset scenarios in kapps example contract
    
    * fix: linting warns
    
    * chore: change scenario response to string

--- vm/src/tx_execution/builtin_function_mocks/general.rs
+++ vm/src/tx_execution/builtin_function_mocks/general.rs
@@ -1,9 +1,11 @@
 mod change_owner_mock;
 mod klever_asset_trigger_mock;
+mod klever_create_asset_mock;
 mod set_account_name_mock;
 mod upgrade_contract;
 
 pub use change_owner_mock::*;
 pub use klever_asset_trigger_mock::*;
+pub use klever_create_asset_mock::*;
 pub use set_account_name_mock::*;
 pub use upgrade_contract::*;
